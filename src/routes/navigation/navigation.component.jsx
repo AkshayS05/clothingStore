09507bb13a4d120f9bf6638b309b1ab93df6d633
akshay <akshaysharma5432@gmail.com>
2022-04-26 14:35:25 +0530

    add navigation

---
+++ src/routes/navigation/navigation.component.jsx
@@ -0,0 +1,23 @@
+import { Fragment } from "react";
+import { Link, Outlet } from "react-router-dom";
+import { ReactComponent as AwesomeLogo } from "../../assets/crown.svg";
+//scss
+import "./navigation.styles.scss";
+const Navigation = () => {
+  return (
+    <Fragment>
+      <div className="navigation">
+        <Link className="logo-container" to="/">
+          <AwesomeLogo className="logo" />
+        </Link>
+        <div className="nav-links-container">
+          <Link className="nav-link" to="/shop">
+            Shop
+          </Link>
+        </div>
+      </div>
+      <Outlet />
+    </Fragment>
+  );
+};
+export default Navigation;
